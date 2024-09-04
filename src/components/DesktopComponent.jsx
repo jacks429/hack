4b65fb8925a02be4026c7134ef15db2afd580005
jacks429 <64680540+jacks429@users.noreply.github.com>
2024-09-04 13:46:09 -0500

    first commit

---
+++ src/components/DesktopComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DesktopComponent = () => {
+  return (
+    <div className="hidden md:block">
+      This is the desktop version.
+    </div>
+  );
+};
+
+export default DesktopComponent;
---
+++ src/components/MobileComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MobileComponent = () => {
+  return (
+    <div className="block md:hidden">
+      This is the mobile version.
+    </div>
+  );
+};
+
+export default MobileComponent;
