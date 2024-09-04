e1f192fd8d002ecb15086aa0e47a8df1866de92c
jacks429 <64680540+jacks429@users.noreply.github.com>
2024-09-04 13:37:21 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `hack`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss"]
+};
