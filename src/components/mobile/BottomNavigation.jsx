e3df38d6e29d26a44dac3c9c65c12c573bdf7022
jacks429 <64680540+jacks429@users.noreply.github.com>
2024-09-05 08:49:53 -0500

    removed not working buttons

--- src/components/mobile/BottomNavigation.jsx
+++ src/components/mobile/BottomNavigation.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import homeIcon from '../../images/home-icon.png';
-import voteIcon from '../../images/vote-icon.png';
 import documentsIcon from '../../images/documents-icon.png';
-import settingsIcon from '../../images/settings-icon.png';
 import servicesIcon from '../../images/services-icon.png';
 
 const BottomNavigation = ({ onPageChange }) => (
@@ -12,12 +10,6 @@ const BottomNavigation = ({ onPageChange }) => (
     </button>
     <button onClick={() => onPageChange('documents')} className="flex flex-col items-center text-gray-500">
       <img src={documentsIcon} alt="Documents" style={{ width: '48px', height: '48px', objectFit: 'cover' }} />
-    </button>
-    <button onClick={() => onPageChange('vote')} className="flex flex-col items-center text-gray-500">
-      <img src={voteIcon} alt="Vote" style={{ width: '48px', height: '48px', objectFit: 'cover' }} />
-    </button>
-    <button onClick={() => onPageChange('news')} className="flex flex-col items-center text-gray-500">
-      <img src={settingsIcon} alt="News" style={{ width: '48px', height: '48px', objectFit: 'cover' }} />
     </button>
     <button onClick={() => onPageChange('services')} className="flex flex-col items-center text-gray-500">
       <img src={servicesIcon} alt="Services" style={{ width: '48px', height: '48px', objectFit: 'cover' }} />
